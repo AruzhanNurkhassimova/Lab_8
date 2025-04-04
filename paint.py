3911b803a7a1eec231242ab7e1c73b8014275fa6
AruzhanNurkhassimova <a_nurkhassimova@kbtu.kz>
2025-04-04 21:16:03 +0500

    1

---
+++ paint.py
@@ -0,0 +1,136 @@
+import pygame
+import sys
+import math
+pygame.init()
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Simple paint')
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+green = (0, 255, 0)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+gray= (200, 200, 200)
+
+class Button:
+    def __init__(self, x, y, width, height, text, color, action):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = color
+        self.action = action
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        font = pygame.font.Font(None, 30)
+        text_surface = font.render(self.text, True, white)
+        screen.blit(text_surface, (self.rect.x + 12, self.rect.y + 12))
+    def check_action(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.action()
+
+drawing = False
+brush_color = black
+
+def set_black():
+    global brush_color
+    brush_color = black
+def set_green():
+    global brush_color
+    brush_color = green
+def set_red():
+    global brush_color
+    brush_color = red
+def set_blue():
+    global brush_color
+    brush_color = blue
+def clear_screen():
+    screen.fill(white)
+def exit_app():
+    pygame.quit()
+    sys.exit()
+
+def drawCircle(screen, start, end, width, color): 
+    x1 = start[0] 
+    x2 = end[0] 
+    y1 = start[1] 
+    y2 = end[1]  
+    
+    x = (x1 + x2) / 2  
+    y = (y1 + y2) / 2  
+    
+    radius = abs(x1 - x2) / 2  
+    
+    pygame.draw.circle(screen, pygame.Color(color), (x, y), radius, width)  
+
+ 
+ 
+def drawRectangle(screen, start, end, width, color): 
+    x1 = start[0]  
+    x2 = end[0]  
+    y1 = start[1]  
+    y2 = end[1]  
+    
+    widthr = abs(x1 - x2)  
+    height = abs(y1 - y2)  
+    
+    if x2 > x1 and y2 > y1: 
+        pygame.draw.rect(screen, pygame.Color(color), (x1, y1, widthr, height), width)  
+    if y2 > y1 and x1 > x2: 
+        pygame.draw.rect(screen, pygame.Color(color), (x2, y1, widthr, height), width)  
+    if x1 > x2 and y1 > y2: 
+        pygame.draw.rect(screen, pygame.Color(color), (x2, y2, widthr, height), width)  
+    if x2 > x1 and y1 > y2: 
+        pygame.draw.rect(screen, pygame.Color(color), (x1, y2, widthr, height), width)
+
+
+buttons = [
+    Button(0, 10, 80, 30, 'Black', black, set_black),
+    Button(100, 10, 80, 30, 'Green', green, set_green),
+    Button(200, 10, 80, 30, 'Red', red, set_red),
+    Button(300, 10, 80, 30, 'Blue', blue, set_blue),
+    Button(400, 10, 80, 30, 'Clear', gray, clear_screen),
+    Button(500, 10, 80, 30, 'Exit', gray, exit_app)
+]
+
+clear_screen()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_r: 
+                mode = 'rectangle'  
+            if event.key == pygame.K_c: 
+                mode = 'circle'  
+            if event.key == pygame.K_p:
+                mode = 'pen'
+        
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                drawing = True
+                if mode == 'rectangle' or mode == 'circle':
+                    prevpos = event.pos
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                drawing = False
+                if mode == 'rectangle':
+                    drawRectangle(screen, prevpos, event.pos, 5, brush_color)
+                if mode == 'circle':
+                    drawCircle(screen, prevpos, event.pos, 5, brush_color)
+        
+        for button in buttons:
+            button.check_action(event)
+    if drawing and mode == 'pen':
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        if mouse_y > 50:
+            pygame.draw.circle(screen, brush_color, (mouse_x, mouse_y), 5)
+
+    pygame.draw.rect(screen, gray, (0, 0, width, 50))
+    for button in buttons:
+        button.draw(screen)
+
+    pygame.display.flip()
